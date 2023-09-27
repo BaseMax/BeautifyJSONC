7a6f098c3cf7508f7ac0a198d365241963049065
Maximilian Edison <maximilianedison@gmail.com>
2023-09-27 19:49:31 +0330

    Create beautify_json.h

---
+++ src/beautify_json.h
@@ -0,0 +1,10 @@
+#ifndef BEAUTIFY_JSON_H
+#define BEAUTIFY_JSON_H
+
+#include <stdio.h>
+
+#define INDENT_SIZE 4
+
+void beautifyJson(FILE *inputFile, FILE *outputFile);
+
+#endif // BEAUTIFY_JSON_H

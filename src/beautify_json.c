59f8c2afa9137e755f707138853306abad051c5e
Maximilian Edison <maximilianedison@gmail.com>
2023-09-27 19:49:02 +0330

    Update beautify_json.c

--- src/beautify_json.c
+++ src/beautify_json.c
@@ -1,17 +1,5 @@
-/*
-*
-*    @Name: Beautify JSON C
-*    @License: GPL-3.0
-*    @Date:     27/09/2023
-*    @Repository:   https://github.com/BaseMax/BeautifyJSONC
-*
-*/
-
+#include "beautify_json.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define INDENT_SIZE 4
 
 void beautifyJson(FILE *inputFile, FILE *outputFile) {
     int indentLevel = 0;
@@ -75,31 +63,3 @@ void beautifyJson(FILE *inputFile, FILE *outputFile) {
         prevChar = c;
     }
 }
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s input_file output_file\n", argv[0]);
-        return 1;
-    }
-
-    FILE *inputFile = fopen(argv[1], "r");
-    if (inputFile == NULL) {
-        perror("Error opening input file");
-        return 1;
-    }
-
-    FILE *outputFile = fopen(argv[2], "w");
-    if (outputFile == NULL) {
-        perror("Error opening output file");
-        fclose(inputFile);
-        return 1;
-    }
-
-    beautifyJson(inputFile, outputFile);
-
-    fclose(inputFile);
-    fclose(outputFile);
-
-    return 0;
-}
-
